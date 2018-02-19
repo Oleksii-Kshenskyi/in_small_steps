157298f7368c366114676399195aba79609c3c97
Oleksii Kshenskyi <archalessus@gmail.com>
2018-02-19 08:18:30 +0200

    [0.0.1.1|16] CALC project: - YES! Finally the pure virtual function call has been fixed. The thing is, QQmlApplicationEngine was being passed as a shared_ptr, even though the variable itself had been previously declared on stack. Because of that, shared_ptr couldn't properly destroy it and that that caused the crash. Will think about some more refactoring and then FINALLY start new feature implementation.

--- 1_CALC_MVC/main.cpp
+++ 1_CALC_MVC/main.cpp
@@ -4,12 +4,11 @@ int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
-    QQmlApplicationEngine engine;
-    engine.load(QUrl(QLatin1String("qrc:/calcui/main.qml")));
-    CLTestBuilder clbBuilder(&app, std::shared_ptr<QQmlApplicationEngine>(&engine)); Q_UNUSED(clbBuilder);
+    std::shared_ptr<QQmlApplicationEngine> engine = std::make_shared<QQmlApplicationEngine>();
+    engine->load(QUrl(QLatin1String("qrc:/calcui/main.qml")));
+    CLTestBuilder clbBuilder(&app, engine); Q_UNUSED(clbBuilder);
 
-
-    if (engine.rootObjects().isEmpty())
+    if (engine->rootObjects().isEmpty())
         return -1;
 
     return app.exec();
