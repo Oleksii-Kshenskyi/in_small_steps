8bd60f21b8a5caddd639ec85295c8f091e48519e
Oleksii Kshenskyi <archalessus@gmail.com>
2018-02-20 13:08:07 +0200

    [0.1.37] EFF MOD CPP project: - Tested array references type deduction:   Created a function that deduces array size on compile time.

--- 2_EFFECTIVE_MODERN_CPP/ITEM_1_UNDERSTAND_TEMPLATE_TYPE_DEDUCTION/emc_item_1/main.cpp
+++ 2_EFFECTIVE_MODERN_CPP/ITEM_1_UNDERSTAND_TEMPLATE_TYPE_DEDUCTION/emc_item_1/main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include <array>
+
+template<typename T, int Size>
+constexpr std::size_t getArraySize(const T(&)[Size]) noexcept
+{
+	return Size;
+}
+
+
+void getArrayType()
+{
+	std::cout << "=== 1: GET ARRAY TYPE:====" << std::endl;
+	int myArr[] = { 2, 3, 8, 12 };
+	std::cout << "size of arbitrary int array (myArr) is: " << getArraySize(myArr) << std::endl;
+
+	int newArr[getArraySize(myArr)];
+	std::cout << "size of arr determined by getArraySize(myArr) is: " << getArraySize(newArr) << std::endl; 
+
+	std::array<int, getArraySize(myArr)> stdArr;
+	std::cout << "size of std::array is: " << stdArr.size() << std::endl;
+}
 
 int main(int argc, int* argv)
 {
-	std::cout << "Hello Item 1!" << std::endl;
+	getArrayType();
 
 	std::cin.get();
 }
