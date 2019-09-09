8fec889618522745839e76aae9bd571ceef25493
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2019-09-09 16:36:10 +0300

    [1.0.9] CALC project: - Clear Entry button naming fixed.

--- 1_CALC_MVC/view/clqmlconnector.cpp
+++ 1_CALC_MVC/view/clqmlconnector.cpp
@@ -12,11 +12,10 @@ CLQmlConnector::CLQmlConnector(QObject *root, std::shared_ptr<QQuickView> mainVi
 
     QObject::connect(this->object, SIGNAL(numberClicked(const QString&)), this, SIGNAL(changeModelTextForDelta(const QString&)));
     QObject::connect(this->object, SIGNAL(offClicked()), qApp, SLOT(quit()));
-    QObject::connect(this->object, SIGNAL(cancelClicked()), this, SIGNAL(resetQmlMainDisplay()));
+    QObject::connect(this->object, SIGNAL(clearEntryClicked()), this, SIGNAL(clearEntryClicked()));
     QObject::connect(this->object, SIGNAL(eraseOneClicked()), this, SIGNAL(eraseOne()));
     QObject::connect(this->object, SIGNAL(operationClicked(const QString&)), this, SIGNAL(operationClicked(const QString&)));
     QObject::connect(this->object, SIGNAL(equalsSignClicked()), this, SIGNAL(equalsSignClicked()));
-
 }
 
 void CLQmlConnector::setQmlMainDisplayText(const QString& newText)
